4eed3470fdeb3c2a37fcba7cd06324e82cba8adb
Ritwik <ritwik@rapidinnovation.dev>
2023-01-17 16:49:12 +0530

    refresh token validity increased

--- src/shared/constant/constant.ts
+++ src/shared/constant/constant.ts
@@ -1,6 +1,6 @@
 export const Constants = {
   JWT_TOKEN_VALIDITY: '24h',
-  JWT_REFRESH_TOKEN_VALIDITY: '24h',
+  JWT_REFRESH_TOKEN_VALIDITY: '30d',
   USER: 'user',
   ADMIN: 'admin',
   CLIENT: 'client'
